fdf09e82122aee37dece28e88411f2f91efbafb4
dsctt <dascott321@gmail.com>
2022-05-03 22:14:54 -0400

    tmp

--- setup.py
+++ setup.py
@@ -98,8 +98,8 @@ class CMakeBuild(build_ext):
 
 
 setuptools.setup(
-    name="magent",
-    version="0.2.2",
+    name="d-magent",
+    version="0.2.3",
     author="Farama Foundation",
     author_email="jkterry@farama.org",
     description="Multi-Agent Reinforcement Learning environments with very large numbers of agents",
